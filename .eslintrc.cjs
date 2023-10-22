cbe14fce324d0151e3d6da68894aafb5fbfdea55
Bliztle <asbjoern.r.e@gmail.com>
2023-10-22 23:43:28 +0200

    cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,6 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
-        // "plugin:@typescript-eslint/strict-type-checked",
         'plugin:@typescript-eslint/recommended-type-checked',
         'plugin:@typescript-eslint/stylistic-type-checked',
         'plugin:react-hooks/recommended',
